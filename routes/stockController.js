b27ef643d82cf7914310b8d827ce9c61b88bfe85
Tim Kharshan <timkharshan@hotmail.com>
2018-01-28 14:55:44 -0800

    first restful routes working

--- routes/stockController.js
+++ routes/stockController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const stockController = express.Router();
+const Stock = require('../models/stock');
 
 stockController.use(function timeLog (req, res, next) {
   console.log('in stock controller /api/stocks');
@@ -12,12 +13,25 @@ stockController.get('/time', function (req, res, next) {
 stockController.get('/:id', (req, res, next)=> {
   res.send(req.params.id);
 });
-stockController.get('/', function(req, res, next) {
-  res.send('stocks');
 
+// get all
+stockController.get('/', function(req, res, next) {
+  Stock.find(function (err, stocks) {
+    if (err) return next(err);
+    res.json(stocks);
+  });
+});
+
+// post
+stockController.post('/', function (req, res, next) {
+  Stock.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
 });
 
 
 
 
+curl -i -X POST -H "Content-Type: application/json" -d '{name:"stock"}' localhost: 3000 / api / stocks
 module.exports = stockController;
