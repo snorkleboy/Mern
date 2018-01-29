849aafe420dd34d7e65f64e1bfe2fa2a8937d81c
Tim Kharshan <timkharshan@hotmail.com>
2018-01-29 13:19:37 -0800

    entry lists printing

--- src/components/setPages/entryPage.jsx
+++ src/components/setPages/entryPage.jsx
@@ -10,6 +10,9 @@ export default class Stocks extends React.Component {
     componentDidMount(){
         this.ensure();
     }
+    componentWillUpdate(prevprops,newprops){
+        console.log('entrypage willupdate',prevprops, newprops);
+    }
     ensure(){
         if (!this.props.gainers) this.props.getGainers();
         if (!this.props.losers) this.props.getLosers();
@@ -17,15 +20,63 @@ export default class Stocks extends React.Component {
         if (!this.props.mostActives) this.props.getMostActives();
     }
     render(){
+        console.log("ENTRYPAGE PROPS.STOCKS",this.props.stocks)
         return(
             <main className='entry'>
                 <div className='lists'>
+                    <div className='gainers'>
                     <h1>gainers</h1>
+                        <List
+                            data={{
+                                'headers': this.getHeaders(this.props.stocks.gainers),
+                                'entries': this.getEntries(this.props.stocks.gainers)
+                            }}
+                        /> 
+                        
+                    </div>
+
+                    <div className='losers'>
                     <h1>losers</h1>
+                        <List
+                            data={{
+                                'headers':this.getHeaders(this.props.stocks.losers),
+                                'entries':this.getEntries(this.props.stocks.losers)
+                            }}
+                        />
+                    </div>
+
+                    <div className='mostActive'>
+                    <h1>most active</h1>
+                        <List
+                            data={{
+                                'headers': this.getHeaders(this.props.stocks.mostActive),
+                                'entries': this.getEntries(this.props.stocks.mostActive)
+                            }}
+                        />
+                    </div>
+
+                    <div className='mostVolume'>
+                    <h1>most volume</h1>
+                        <List
+                            data={{
+                                'headers': this.getHeaders(this.props.stocks.mostVolume),
+                                'entries': this.getEntries(this.props.stocks.mostVolume)
+                            }}
+                        />
+                    </div>
                 </div>
                 
             </main>
             
         )
+    }
+    getEntries(array){
+        return array;
+    }
+    getHeaders(array){
+        return (array && !array.length == 0) ? 
+            Object.keys(array[0])
+        :
+            [];
     }
 }
--- src/components/stocks/list.jsx
+++ src/components/stocks/list.jsx
@@ -6,23 +6,18 @@ export default class List extends React.Component {
     constructor(props) {
         super(props);
         console.log('list comp',props);
-        this.state = {};
-        try {
-            this.state={'headers': props.data.headers,'entries': props.data.entries};
-        } catch (error) {
-            console.log('List.jsx error:', 'list must be passed props.data.headers and props.data.entries');
-            console.log(error);
-            this.state={'headers':[],'entries':[]};
+        if (this.props.data === undefined) {
+            console.log('List.jsx error:', 'list must be passed props.data.headers and props.data.entries');            
         }
-        console.log('list state', this.state);
         // this.state.headers=['ticker','price','volume'];
         // this.state.entries = [['aapl', 123, 432234], ['zaag', 321, 678686]];
         
         
     }
+    
 
-    tableMaker(headers = this.state.headers, entries = this.state.entries) {
-
+    tableMaker(headers = this.props.data.headers || [], entries = this.props.data.entries || []) {
+        console.log('tablemaker',this.props, headers,entries);
         return (
             <tbody>
                 <tr>
@@ -30,7 +25,7 @@ export default class List extends React.Component {
                 </tr>
                 {entries.map((entry) => (
                     <tr>
-                        {entry.map((data) => <th>{data}</th>)}
+                        {Object.values(entry).map((data) => <th>{data}</th>)}
                     </tr> 
                 )
                 )}
