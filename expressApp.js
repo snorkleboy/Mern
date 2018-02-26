e9bc1733bc05e99b480b7ad13689b9340cb3ec29
Artem Kharshan <timkharshan@hotmail.com>
2018-02-26 01:26:15 -0800

    mongo heroku, looks for connection details in env

--- expressApp.js
+++ expressApp.js
@@ -3,18 +3,39 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const http = require('http');         // For serving a basic web page.
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 const router = require('./routes/router');
 // const Fetcher = require('./bin/fetcher/fetcher');
 const app = express();
 
 
+    // Here we find an appropriate database to connect to, defaulting to
+    // localhost if we don't find one.
+    const uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/mern-crud';
+
+    // The http server will listen to an appropriate port, or default to
+    // port 5000.
+    const theport = process.env.PORT || 5000;
+
+    // Makes connection asynchronously.  Mongoose will queue up database
+    // operations and release them when the connection is complete.
+
 //mongo
 //
-const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost/mern-crud', { promiseLibrary: require('bluebird') })
-  .then(() =>  console.log('Mongod'))
-  .catch((err) => console.error(err));
+
+mongoose.connect(uristring, { promiseLibrary: require('bluebird') }, function (err, res) {
+      if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      } else {
+      console.log ('Succeeded connected to: ' + uristring);
+      }
+    });
+
 
   //middleware = function((req,res,next)=>{}) express() and express().router are valid middlewares
 app.use(logger('dev'));
