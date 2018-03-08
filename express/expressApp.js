ff5a2c082bfb2ab5811c6fa9b43c7b7cd0e04072
Artem Kharshan <timkharshan@hotmail.com>
2018-03-07 18:35:20 -0800

    reading post params

--- express/expressApp.js
+++ express/expressApp.js
@@ -48,7 +48,7 @@ app.use(cookieSession({
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ 'extended': 'false' }));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
 // app.use(Fetcher);
 app.use(router);
 app.use(express.static(path.join(__dirname, '../build')));
--- express/routes/userController.js
+++ express/routes/userController.js
@@ -11,34 +11,21 @@ userController.use(function timeLog(req, res, next) {
     next();
 });
 userController.get('/', (req, res, next) => {
-    const user = { 'username': "usernamee", 'password': "passwordd" }
-    User.create(user,(error, newUser)=>{
-        if (error){
-            res.json(error)
-        }
-        else{
-            res.json(newUser)
-        }
-    })
-
-    // Users.find({}, (err, data) => res.json(data))
-    console.log('user controller get 2')
+    User.find({}, (err, data) => res.json(data))
 
 });
 // create user
 userController.post('/', (req, res, next) => {
-    // Mosts.Gainer.find({}, (error, data) => {
-    //     res.json(data);
-    // }).select('-_id -created_at -__v');
-    console.log("user.ceate")
-    Users.create({'username':"usernamee",'password':"passwordd"},(error,mystery) => {console.log(error,mystery);res.json({'err':error,'thing':mystery})})
+    console.log(req.body);
+    
+    // Users.create({'username':"usernamee",'password':"passwordd"},(error,mystery) => {console.log(error,mystery);res.json({'err':error,'thing':mystery})})
     // post request with uername and password makes new account and responds with sucess message && session token
 
 });
 
 // check if username is registered
 userController.get('/:username', (req, res, next) => {
-
+    res.json({"input":req.params["username"]})
 });
 
 // login
