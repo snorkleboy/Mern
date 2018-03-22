ad39ddb8289d82ef4919a8c8a39787a0a4e9f3e1
Artem Kharshan <timkharshan@hotmail.com>
2018-03-21 22:53:53 -0700

    styling

--- src/components/rss/iexNews.jsx
+++ src/components/rss/iexNews.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import '../../css/iex-news.css';
 
 export default function({article}){
-
+    function display(e){
+        console.log("HERE",this);
+        const el = document.getElementById(article.headline)
+        el.classList.add("article-visible")
+        setTimeout(() => el.classList.add("article-transition"),0);
+        
+    }
+    function unDisplay(e){
+        const el = document.getElementById(article.headline)
+        el.classList.remove("article-visible");
+        el.classList.remove("article-transition");
+    }
     return (
         <article className="iex-article">
-            <a href={article.url}>
-                <span>{article.headline}</span>
-                <span className="article-tooltip">
+            <a onMouseEnter={display} onMouseLeave={unDisplay} href={article.url}>
+                <span>  --  {article.headline}</span>
+                <span id={article.headline} onMouseLeave={unDisplay} className="article-tooltip">
                     {Date(article.datetime).toLocalTimeString}
                     {article.source}
                     {article.summary}
